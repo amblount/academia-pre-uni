eb3b1ef3416726cf48a33b89980a9b54372ecdb8
Alivia Blount <aliviablount@gmail.com>
2016-09-28 12:30:16 -0700

    add facebook login

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyARsvXHJj7nGLyFTAOW9hqhS7aUUv9si9E",
+    authDomain: "academia-de-mi-abuela-bfef6.firebaseapp.com",
+    databaseURL: "https://academia-de-mi-abuela-bfef6.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+})
